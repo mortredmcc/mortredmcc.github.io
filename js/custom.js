bd6f152c427cf0071a5d47f8c05c0c03f7b2302d
vun <bordin@xp-link.com>
2016-01-13 17:56:32 +0700

    Update hightlight menu.

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,12 @@ $(document).ready(function(){
 	// 	$(this).css('outline','0');    
 	// });
 
+	$('#brand').click(function(){
+		$("#mainMenu li a").removeClass('activeC');
+		$("#home").addClass('activeC');
+		$("#home").css('outline','0');
+	});
+
 	$("#mainMenu li a").click(function(){
 		$("#mainMenu li a").removeClass('activeC');
 		$(this).addClass('activeC');
