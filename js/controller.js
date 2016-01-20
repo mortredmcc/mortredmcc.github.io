0a73a85a3b5e374be6ce5b0ecc0967303eef5852
vun <bordin@xp-link.com>
2016-01-20 14:52:45 +0700

    Update map.

--- js/controller.js
+++ js/controller.js
@@ -5,23 +5,33 @@ DorgkumCtrl.controller('HomeCtrl', function ($scope) {
 })
 .controller('ProductCtrl', function($scope, $http){
 	$scope.greeting = "Product : greetings friend";
-	/*$scope.myInterval = 4000;
-	$scope.noWrapSlides = false;
-	var slides = $scope.slides = [];
-	var url = 'data/img.json';
-	$http.get(url).success( function(response) {
-      $scope.slides = response; 
-   });*/
-
 	$("#owl-demo").owlCarousel({
 	    items : 3,
 	    autoPlay: true,
 	    lazyLoad : true,
 	  });
-
-	  // navigation : true
-
 })
 .controller('ContactUsCtrl', function($scope, $http){
 	$scope.greeting = "Contact US : greetings friend";
+
+	$scope.iniMap = function(){
+		var myOptions = {
+			zoom:18,center:new google.maps.LatLng(13.786476,100.66644),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
+		marker = new google.maps.Marker({
+			map: map,position: new google.maps.LatLng(13.786476,100.66644)
+		});
+		infowindow = new google.maps.InfoWindow({
+			content:'<strong>Dorgkum</strong><br>Serithai<br>10240 Bangkok<br>'
+		});
+		google.maps.event.addListener(marker, 'click', function(){
+			infowindow.open(map,marker);
+		});
+	};
+
+	$scope.initGoogleMap = function(){
+		google.maps.event.addDomListener(window, 'load', $scope.iniMap);
+	};
 });
--- js/custom.js
+++ js/custom.js
@@ -19,24 +19,6 @@ $(document).ready(function(){
 		return; 
 	});
 
-	google.maps.event.addDomListener(window, 'load', init_map);
 });
-
-function init_map(){
-	var myOptions = {
-		zoom:18,center:new google.maps.LatLng(13.786476,100.66644),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
-	map = new google.maps.Map(document.getElementById('gmap_canvas'), myOptions);
-	marker = new google.maps.Marker({
-		map: map,position: new google.maps.LatLng(13.786476,100.66644)
-	});
-	infowindow = new google.maps.InfoWindow({
-		content:'<strong>Dorgkum</strong><br>Serithai<br>10240 Bangkok<br>'
-	});
-	google.maps.event.addListener(marker, 'click', function(){
-		infowindow.open(map,marker);
-	});
-	infowindow.open(map,marker);}
 
 
